09f80a7d9c5acb68e55c2f064863b3c0bffe69a4
Rithikeshh <rithikeshh@gmail.com>
2023-12-01 16:00:28 +0530

    updated commit

--- src/app.js
+++ src/app.js
@@ -12,6 +12,23 @@ app.use(express.json())
 
 // GET endpoint for sending the products to client by id
 //Endpoint - /api/v1/names/:id
+app.get("/api/v1/names/:id",(req, res)=>{
+
+    const {id} = req.params;
+    const product = productNames.find((product)=>{
+        return product.id == id;
+    })
+    if(!product){
+        return res.status(404).json({"status": "failed", "message": "Not found!"})
+    }
+    res.status(200).json({
+        "status": "success",
+        "message": "Product name fetched successfully",
+        "data": {
+            "product": product
+        }
+    })
+})
 
 
 module.exports = app;
